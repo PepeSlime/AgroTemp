f95bdccf0b473ccb0dd9261b4036e169707b2425
PepeSlime <pepegemeo8@gmail.com>
2025-03-26 20:17:12 -0300

    Correção para Vercel

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Servidor AgroTemp rodando!');
+  res.send('Servidor AgroTemp rodando!');
 });
 
-const port = 3000;
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
