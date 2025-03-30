fcaecc42245c3de11185abb123cb29670d624944
PepeSlime <pepegemeo8@gmail.com>
2025-03-30 18:09:24 -0300

    alteração1-supa

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -1,7 +1,13 @@
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+require('dotenv').config(); // Garante que as variáveis de ambiente sejam carregadas
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('As variáveis SUPABASE_URL e SUPABASE_KEY são obrigatórias!');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
-
